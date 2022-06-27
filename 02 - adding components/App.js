5067262acd997f3c9c9990f25860b86ddcea005b
gilad8275 <94356949+gilad8275@users.noreply.github.com>
2022-06-27 13:15:10 +0300

    Add files via upload

---
+++ 02 - adding components/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Layout/Header";
+
+import Meals from "./components/Meals/Meals";
+
+import React, { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
