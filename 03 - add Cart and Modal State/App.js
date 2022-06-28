609d5e04868a0e1d096b27effd057e5d94cfdca3
gilad8275 <94356949+gilad8275@users.noreply.github.com>
2022-06-28 11:37:09 +0300

    Add files via upload

---
+++ 03 - add Cart and Modal State/App.js
@@ -0,0 +1,31 @@
+import Header from "./components/Layout/Header";
+
+import Meals from "./components/Meals/Meals";
+
+import React, { Fragment, useState } from 'react';
+
+import Cart from "./components/Cart/Cart";
+
+function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  };
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
